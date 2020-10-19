7c91db10fcc5563ec9d3b9f20dd36a998206af42
marconardin <27.marco@gmail.com>
2020-10-19 17:08:07 -0400

    Create cell effects and control buttons

--- src/script.js
+++ src/script.js
@@ -1,12 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
+  let random = true;
+
   createBoard();
-  randomizeCells();
+  if (random) randomizeCells();
+  clickCellOnOff();
+  createButtons();
 });
 
 function createBoard() {
-  console.log("test");
   const N = 10;
   const tbl = document.createElement("table");
+  tbl.style.margin = "20px auto 20px auto";
   document.body.appendChild(tbl);
   document.body.appendChild(document.createElement("p"));
   for (let i = 0; i < N; i++) {
@@ -18,12 +22,47 @@ function createBoard() {
     }
   }
 }
-/*
+
 function randomizeCells() {
-    const cells = document.querySelectorAll('td');
-    newCells = cells.map(cell => {
-        // 50% chance to fill cell
-        const filled = Math.floor(Math.random() * Math.floor(2));
-    });
+  const cells = document.querySelectorAll("td");
+  cells.forEach((cell) => {
+    // 25% chance to fill cell
+    const filled = Math.random() < 0.25;
+    if (filled) {
+      cell.classList.add("filled");
+    }
+  });
 }
-*/
+
+function clickCellOnOff() {
+  const cells = document.querySelectorAll("td");
+  cells.forEach((cell) => {
+    cell.addEventListener("click", function () {
+      this.classList.toggle("filled");
+      this.style.backgroundColor = "";
+    });
+    cell.addEventListener("mouseover", function () {
+      this.style.backgroundColor = "pink";
+    });
+    cell.addEventListener("mouseout", function () {
+      this.style.backgroundColor = "";
+    });
+  });
+}
+
+function createButtons() {
+  const btns = ['START', 'STOP', 'RESET'];
+  const btnBox = document.createElement('div');
+  document.body.appendChild(btnBox);
+  btnBox.style.margin = "auto";
+  btnBox.style.padding = "1rem";
+  btnBox.style.display = "flex";
+  btnBox.style.justifyContent = "center";
+  for (let btnText of btns) {
+    const startBtn = document.createElement("button");
+    startBtn.style.height = "50px";
+    startBtn.style.width = "100px";
+    startBtn.innerText = btnText;
+    btnBox.appendChild(startBtn);
+  }
+}
