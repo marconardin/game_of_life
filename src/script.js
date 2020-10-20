468ba0b4e8ffc085344132f4fa9ea69bd7cf4307
marconardin <27.marco@gmail.com>
2020-10-20 16:53:49 -0400

    Implement basic game loop

--- src/script.js
+++ src/script.js
@@ -1,16 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   let random = true;
+  // TODO: add preset configurations, not just random
 
   createBoard();
   if (random) randomizeCells();
   clickCellOnOff();
   createButtons();
 
-  // setInterval(runGame, 1000);
+  setInterval(runGame, 500);
 });
 
 function createBoard() {
-  const N = 10;
+  const N = 50;
   const tbl = document.createElement("table");
   tbl.style.margin = "20px auto 20px auto";
   document.body.appendChild(tbl);
@@ -29,9 +30,9 @@ function randomizeCells() {
   const cells = document.querySelectorAll("td");
   cells.forEach((cell) => {
     // 25% chance to fill cell
-    const filled = Math.random() < 0.25;
-    if (filled) {
-      cell.classList.add("filled");
+    const alive = Math.random() < 0.25;
+    if (alive) {
+      cell.classList.add("alive");
     }
   });
 }
@@ -40,7 +41,7 @@ function clickCellOnOff() {
   const cells = document.querySelectorAll("td");
   cells.forEach((cell) => {
     cell.addEventListener("click", function () {
-      this.classList.toggle("filled");
+      this.classList.toggle("alive");
       this.style.backgroundColor = "";
     });
     cell.addEventListener("mouseover", function () {
@@ -55,6 +56,7 @@ function clickCellOnOff() {
 function createButtons() {
   const btns = ["START", "STOP", "RESET"];
   const btnBox = document.createElement("div");
+  btnBox.classList.add("button-box");
   document.body.appendChild(btnBox);
   btnBox.style.margin = "auto";
   btnBox.style.padding = "1rem";
@@ -70,13 +72,42 @@ function createButtons() {
 }
 
 function runGame() {
-  const cells = document.querySelectorAll('td');
+  const table = document.querySelector("table");
+  const cells = document.querySelectorAll("td");
+
   for (let cell of cells) {
-    // if alive
-    //  if 2-3 neighbors, stay alive
-    //  else, die
-    // else (dead)
-    //  if 3 neighbors, born
-    //  else, stay dead
+    const rowIndex = cell.parentElement.rowIndex;
+    const cellIndex = cell.cellIndex;
+
+    // determine number of alive neighbors
+    let aliveNeighbors = 0;
+    for (let i = rowIndex - 1; i <= rowIndex + 1; i++) {
+      for (let j = cellIndex - 1; j <= cellIndex + 1; j++) {
+        if (rowIndex == i && cellIndex == j) continue;
+        if (
+          i >= 0 &&
+          i < table.rows.length &&
+          j >= 0 &&
+          j < table.rows[0].cells.length
+        ) {
+          if (table.rows[i].cells[j].classList.contains("alive"))
+            aliveNeighbors++;
+        }
+      }
+    }
+
+    // update the next state of the cell
+    if (cell.classList.contains("alive")) {
+      cell.nextAlive =
+        aliveNeighbors == 2 || aliveNeighbors == 3 ? true : false;
+    } else {
+      cell.nextAlive = aliveNeighbors == 3 ? true : false;
+    }
+  }
+
+  // update the table values
+  for (let cell of cells) {
+    if (cell.nextAlive) cell.classList.add("alive");
+    if (!cell.nextAlive) cell.classList.remove("alive");
   }
 }
